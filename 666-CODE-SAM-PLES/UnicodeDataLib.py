3929586cc79135d511a9c2afc895475c9c40259c
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-03-26 19:55:52 +0330

    # fun shit #

--- 666-CODE-SAM-PLES/UnicodeDataLib.py
+++ 666-CODE-SAM-PLES/UnicodeDataLib.py
@@ -10,3 +10,18 @@ def unicode_test(value):
 
 
 unicode_test('@')
+unicode_test('\u2603') #=> value="☃", name="SNOWMAN", value2="☃"
+#unicode_test('A') #=> value="A", name="LATIN CAPITAL LETTER A", value2="A
+#unicode_test('$') #=> value="$", name="DOLLAR SIGN", value2="$"
+
+
+
+
+                # fun shit #
+#  Encoding name         Description
+#  'ascii'              Good old seven-bit ASCII
+#  'utf-8'              Eight-bit variable-length encoding, and what you almost always want to use
+#  'latin-1'            Also known as ISO 8859-1
+#  'cp-1252'            A common Windows encoding
+#  'unicode-escape'     Python Unicode literal format, `\u`xxxx or `\U`xxxxxxxx
+                # fun shit #
